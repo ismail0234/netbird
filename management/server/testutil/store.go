a5434e67b9b31a608c907a382249bd049c343e1e
İsmail <ismaiil_0234@hotmail.com>
2024-11-06 14:49:42 +0300

    Update store.go

--- management/server/testutil/store.go
+++ management/server/testutil/store.go
@@ -39,7 +39,7 @@ func CreateMyDB() (func(), error) {
 
 	ctx := context.Background()
 	c, err := mysql.Run(ctx,
-		"mysql:8.0.40",
+		"mysql:5.7.34",
 		mysql.WithDatabase("netbird"),
 		mysql.WithUsername("netbird"),
 		mysql.WithPassword("mysql"),

9320ab27e83f261bd11859ac5a00faf5482d2bb4
İsmail <ismaiil_0234@hotmail.com>
2024-11-09 12:15:51 +0300

    UP

--- management/server/testutil/store.go
+++ management/server/testutil/store.go
@@ -38,7 +38,7 @@ func CreateMyDB() (func(), error) {
 	mysqlConfigPath := "../../management/server/testdata/mysql.cnf"
 
 	ctx := context.Background()
-	c, err := mysql.Run(ctx,
+	c, err := RunMySQL(ctx,
 		"mysql:8.0.40",
 		mysql.WithConfigFile(mysqlConfigPath),
 		mysql.WithDatabase("netbird"),

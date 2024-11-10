cb650c0dbda9f5de7d6f214962349cd08a08dc86
İsmail <ismaiil_0234@hotmail.com>
2024-11-10 11:40:07 +0300

    up

--- management/server/testutil/store.go
+++ management/server/testutil/store.go
@@ -8,12 +8,12 @@ import (
 	"os"
 	"time"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/mysql"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
 	mysqlGorm "gorm.io/driver/mysql"
+	postgresGorm "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -31,6 +31,13 @@ func emptyCleanUp() {
 
 func CreatePostgresTestContainer() (func(), error) {
 	if postgresContainer != nil && postgresContainer.IsRunning() && postgresContainerString != "" {
+
+		db, err := gorm.Open(postgresGorm.Open(postgresContainerString))
+		if err != nil {
+			return nil, err
+		}
+
+		RefreshContainer(db, "NETBIRD_STORE_ENGINE_POSTGRES_DSN", postgresContainerString)
 		return emptyCleanUp, nil
 	}
 
@@ -43,15 +50,15 @@ func CreatePostgresTestContainer() (func(), error) {
 		return nil, err
 	}
 
-	talksConn, err := container.ConnectionString(ctx)
+	talksConn, _ := container.ConnectionString(ctx)
 
 	postgresContainer = container
 	postgresContainerString = talksConn
 
-	return GetContextDB(ctx, container, talksConn, err, "NETBIRD_STORE_ENGINE_POSTGRES_DSN", false)
+	return emptyCleanUp, os.Setenv("NETBIRD_STORE_ENGINE_POSTGRES_DSN", talksConn)
 }
 
-func CreateOrGetMysqlTestContainer() (func(), error) {
+func CreateMysqlTestContainer() (func(), error) {
 
 	if mysqlContainer != nil && mysqlContainer.IsRunning() && mysqlContainerString != "" {
 
@@ -60,7 +67,7 @@ func CreateOrGetMysqlTestContainer() (func(), error) {
 			return nil, err
 		}
 
-		RefreshDatabase(db, "NETBIRD_STORE_ENGINE_MYSQL_DSN", mysqlContainerString)
+		RefreshContainer(db, "NETBIRD_STORE_ENGINE_MYSQL_DSN", mysqlContainerString)
 		return emptyCleanUp, nil
 	}
 
@@ -77,17 +84,17 @@ func CreateOrGetMysqlTestContainer() (func(), error) {
 		return nil, err
 	}
 
-	talksConn, err := container.ConnectionString(ctx)
+	talksConn, _ := container.ConnectionString(ctx)
 
 	os.Setenv("NB_SQL_MAX_OPEN_CONNS", "20")
 
 	mysqlContainer = container
 	mysqlContainerString = talksConn
 
-	return GetContextDB(ctx, container, talksConn, err, "NETBIRD_STORE_ENGINE_MYSQL_DSN", true)
+	return emptyCleanUp, os.Setenv("NETBIRD_STORE_ENGINE_MYSQL_DSN", talksConn)
 }
 
-func RefreshDatabase(db *gorm.DB, dsn string, connectionString string) {
+func RefreshContainer(db *gorm.DB, dsn string, connectionString string) {
 	db.Exec("DROP DATABASE netbird")
 	db.Exec("CREATE DATABASE netbird")
 
@@ -95,45 +102,4 @@ func RefreshDatabase(db *gorm.DB, dsn string, connectionString string) {
 	sqlDB.Close()
 
 	os.Setenv(dsn, connectionString)
-}
-
-func CreatePGDB() (func(), error) {
-
-	ctx := context.Background()
-	c, err := postgres.Run(ctx, "postgres:16-alpine", testcontainers.WithWaitStrategy(
-		wait.ForLog("database system is ready to accept connections").
-			WithOccurrence(2).WithStartupTimeout(15*time.Second)),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	talksConn, err := c.ConnectionString(ctx)
-
-	return GetContextDB(ctx, c, talksConn, err, "NETBIRD_STORE_ENGINE_POSTGRES_DSN", false)
-}
-
-func GetContextDB(ctx context.Context, c testcontainers.Container, talksConn string, err error, dsn string, clearCleanUp bool) (func(), error) {
-
-	cleanup := func() {
-		timeout := 10 * time.Second
-		err = c.Stop(ctx, &timeout)
-		if err != nil {
-			log.WithContext(ctx).Warnf("failed to stop container: %s", err)
-		}
-	}
-
-	if clearCleanUp {
-		cleanup := func() {
-			_ = 1
-		}
-
-		return cleanup, os.Setenv(dsn, talksConn)
-	}
-
-	if err != nil {
-		return cleanup, err
-	}
-
-	return cleanup, os.Setenv(dsn, talksConn)
 }

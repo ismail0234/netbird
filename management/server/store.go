ed48c17ade26ee3a871f0572e509e78c0657bb7b
İsmail <ismaiil_0234@hotmail.com>
2024-11-10 14:45:31 +0300

    Update store.go

--- management/server/store.go
+++ management/server/store.go
@@ -310,7 +310,7 @@ func getSqlStoreEngine(ctx context.Context, store *SqlStore, kind StoreEngine) (
 	}
 
 	if kind == MysqlStoreEngine {
-		cleanUp, err := testutil.CreateMysqlTestContainer()
+		cleanUp, err := testutil.CreateMyDB()
 		if err != nil {
 			return nil, nil, err
 		}
